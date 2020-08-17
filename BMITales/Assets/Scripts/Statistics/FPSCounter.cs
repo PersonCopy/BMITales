6caabff3fd52ecfed91ff7d8ff6cb50674c3fa31
Adrian Habeler <adrian.habeler@gmail.com>
2020-08-17 19:50:00 +0200

    added player, terrain

---
+++ BMITales/Assets/Scripts/Statistics/FPSCounter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public Text fpsLabel;
+    float lastUpdateTime = 0;
+
+    public void Update()
+    {
+        if (lastUpdateTime + .5f <= Time.time)
+		{
+            int current = (int)(1f / Time.unscaledDeltaTime);
+            fpsLabel.text = $"{current} FPS";
+            lastUpdateTime = Time.time;
+		}
+    }
+}
